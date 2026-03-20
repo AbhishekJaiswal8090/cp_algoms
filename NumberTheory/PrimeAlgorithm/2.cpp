6b0c35acb10f95f738f96f133e0260441d438ef2
Abhishek <abhi84514aaa.in@gmail.com>
2026-03-20 13:17:07 +0530

    Prime factors

---
+++ NumberTheory/PrimeAlgorithm/2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Prime factorisation
+// IDEA
+// Break down n into its prime factors by repeatedly dividing by the smallest possible primes
+
+// Algorthm
+vector<int> Factors(int n)
+{
+    vector<int> facts;
+    for (int i = 2; i * i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            facts.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        facts.push_back(n);
+    return facts;
+}
+
+int main()
+{
+}
