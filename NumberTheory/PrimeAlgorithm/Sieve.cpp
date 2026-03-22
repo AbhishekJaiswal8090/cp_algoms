139df7a8cb59c599561eeb5a36e0a9f104d7b24f
Abhishek <abhi84514aaa.in@gmail.com>
2026-03-22 15:16:34 +0530

    Optimized Sieve

--- NumberTheory/PrimeAlgorithm/Sieve.cpp
+++ NumberTheory/PrimeAlgorithm/Sieve.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // Sieve of Eratosthenes
@@ -6,10 +7,52 @@ using namespace std;
 
 // Key Idea
 // Start with a list of numbers from 2 to n
-// Mark multiplies of each prime number from 2 as composite
+// Mark multiples of each prime number from 2 as composite
 
+vector<bool> Sieve(int n)
+{
 
+    vector<bool> a(n + 1, true);
+    a[0] = a[1] = false;
+    for (int i = 2; i <= n; i++)
+    {
+        if (a[i])
+        {
+            for (int j = 2 * i; j <= n; j += i)
+            {
+                a[j] = false;
+            }
+        }
+    }
+    return a;
+}
 
+// Time Complexity o(nloglogn) almost linear
+// Space Complexity o(n)
+
+// The standard sieve has two limitations
+// 1 -> Memory access patterns are cacheinefficient
+// 2 -> Memory usage becomes problematic for very large n
+
+// Here are two potimzations we ccan make;
+// Only mark multipples of primes upto root(n) beacuse
+// All remaining unmarked numbers are primes
+
+void OptimizedSieve(int n)
+{
+    vector<bool> a(n + 1, true);
+    a[0] = a[1] = false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (a[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                a[j] = false;
+            }
+        }
+    }
+}
 int main()
 {
 }
