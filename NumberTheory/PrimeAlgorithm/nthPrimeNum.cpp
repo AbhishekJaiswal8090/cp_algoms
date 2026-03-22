676705dfcc926a40ad061dbd88a4c5e144e27f73
Abhishek <abhi84514aaa.in@gmail.com>
2026-03-22 18:04:48 +0530

    nth prime number soln

---
+++ NumberTheory/PrimeAlgorithm/nthPrimeNum.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// In this lecture we are going to solve the problem finding nth prime number
+// And here we are using optimized sieve algorithm
+
+void solve(int n)
+{
+    if (n == 1)
+        cout << 2 << endl;
+    return;
+    vector<bool> a(n + 1, true);
+
+    // Handline even numbers
+    for (int i = 4; i <= n; i += 2)
+    {
+        a[i] = false;
+    }
+
+    // handling odd numbers
+    for (int i = 3; i <= n; i += 2)
+    {
+        if (a[i])
+        {
+            for (int j = i * i; j <= n; j += 2 * i)
+            {
+                a[i] = false;
+            }
+        }
+    }
+    int cnt = 1; // we already counted 2
+    for (int i = 3; i <= n; i += 2)
+    {
+        if (a[i])
+            cnt++;
+        if (cnt == n)
+        {
+            cout << i << endl;
+            break;
+        }
+    }
+
+    return;
+}
+
+int main()
+{
+}
