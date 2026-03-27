2c59fe237a72e4c6be0d0b8be1a4d082a2f4d58d
Abhishek <abhi84514aaa.in@gmail.com>
2026-03-27 23:35:30 +0530

    Explanation

--- BinarySearch/1.cpp
+++ BinarySearch/1.cpp
@@ -4,7 +4,8 @@ using namespace std;
 // IN today lecture we are going to study about Binary Search on answer
 // Its not just finding elements from any given array but its more 
 // like findind a paritcular candidate answer which satisfies a sort of condition
-// Thats why its
+// Thats why its known as Binary Search on answer
+
 
 
 int main(){
