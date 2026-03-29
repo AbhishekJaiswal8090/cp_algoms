4375ec0265a9766a328c6f30f23eb82946737464
Abhishek <abhi84514aaa.in@gmail.com>
2026-03-29 23:37:20 +0530

    implementation of lower and upperbound functions

--- BinarySearch/Basic/2.cpp
+++ BinarySearch/Basic/2.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+// IN this lecture we are going to study
+// upper and lower bound
+// lower bound is defined as the element >= the given element
+// While upper bound represents element > the given element
 
-
-int main(){
-
-
+int main()
+{
 }
